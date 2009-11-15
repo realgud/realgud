8c4479e549511c1fe9a454db10fb848e86819b64
rocky <rockyb@rubyforge.org>
2009-11-15 04:43:33 -0500

    Start routines which allow us to send commands to the debugger process: dbgr-send-command. Right now though, It is not inserting into the buffer nor the output filter function to kick in.

--- dbgr-buffer.el
+++ dbgr-buffer.el
@@ -4,20 +4,31 @@
 
 (defvar dbgr-cmdbuf-info)
 
+(defun dbgr-get-cmdbuf-from-scriptbuf ( &optional opt-buffer)
+  "Return the command buffer associated with source
+OPT-BUFFER or if that is ommited `current-buffer' which is
+assumed to be a source file buffer."
+  (let ((buffer (or opt-buffer (current-buffer))))
+    (if (dbgr-scriptbuf? buffer)
+	(with-current-buffer buffer
+	  (dbgr-scriptbuf-info-cmdproc dbgr-scriptbuf-info))
+      nil)))
+
 (defun dbgr-get-scriptbuf-from-cmdbuf ( &optional opt-buffer)
-  "Return the script buffer associated with command
+  "Return the source buffer associated with command
 OPT-BUFFER or if that is ommited `current-buffer' which is
 assumed to be a process command buffer."
   (let ((buffer (or opt-buffer (current-buffer))))
-    (with-current-buffer buffer
-      (let ((loc (dbgr-loc-hist-item (dbgr-cmdbuf-loc-hist dbgr-cmdbuf-info))))
-	(if loc
-	    (marker-buffer (dbgr-loc-marker loc))
-	  nil)
-	)))
-)
+    (if (dbgr-cmdbuf? buffer)
+	(with-current-buffer buffer
+	  (let ((loc (dbgr-loc-hist-item (dbgr-cmdbuf-loc-hist dbgr-cmdbuf-info))))
+	    (if loc
+		(marker-buffer (dbgr-loc-marker loc))
+	      nil)
+	    ))
+      nil)))
 
-(defun dbgr-scriptbuf-get( &optional opt-buffer)
+(defun dbgr-get-scriptbuf( &optional opt-buffer)
   "Return the script buffer associated with OPT-BUFFER
 or `current-buffer' if that is omitted. nil is returned
 if we don't find anything."
@@ -25,9 +36,26 @@ if we don't find anything."
   (let ((buffer (or opt-buffer (current-buffer))))
     (with-current-buffer buffer
       (cond 
-       ;; Perhaps buffer is a script buffer.
+       ;; Perhaps buffer is a source script buffer?
        ((dbgr-scriptbuf? buffer) buffer)
        ;; Perhaps buffer is a process command buffer.
        ((dbgr-cmdbuf? buffer)
 	(dbgr-get-scriptbuf-from-cmdbuf buffer))
        (t nil)))))
+
+(defun dbgr-get-cmdbuf( &optional opt-buffer)
+  "Return the script buffer associated with OPT-BUFFER
+or `current-buffer' if that is omitted. nil is returned
+if we don't find anything."
+
+  (let ((buffer (or opt-buffer (current-buffer))))
+    (with-current-buffer buffer
+      (cond 
+       ;; Perhaps buffer is a process command buffer?
+       ((dbgr-cmdbuf? buffer) buffer)
+       ;; Perhaps buffer is a source script buffer.
+       ((dbgr-scriptbuf? buffer)
+	(dbgr-get-cmdbuf-from-scriptbuf buffer))
+       (t nil)))))
+
+(provide 'dbgr-buffer)
---
+++ dbgr-send.el
@@ -0,0 +1,14 @@
+(require 'load-relative)
+(require-relative-list
+ '("dbgr-buffer"))
+
+(defun dbgr-send-command (command &optional opt-buffer)
+  "Invoke the debugger COMMAND displaying."
+  (let* ((buffer (or opt-buffer (current-buffer)))
+	 (cmdbuf (dbgr-get-cmdbuf buffer)))
+    (if cmdbuf
+	(let ((proc (get-buffer-process cmdbuf)))
+	  (or proc (error "Current buffer has no process"))
+	  (process-send-string proc (concat command "\n"))))))
+
+(provide 'dbgr-send)
