d4097faf62f26b2599d266706d87561b70a8496a
rocky <rockyb@rubyforge.org>
2009-11-15 11:56:16 -0500

    Add with-current-buffer-safe to guard against deleted buffers. Set/save comint-prompt-regexp. Correct debugger prompts which are not the same as location matching patterns. Workaround problem in loading pydbg/rbdbgr not pulling in all files. Include python regexp test.

--- test/test-helper.el
+++ test/test-helper.el
@@ -5,26 +5,51 @@
 
 (behave-clear-contexts)
 
-
 (context 
  "helper functions"
  (tag helper)
  (specify "fn-p-to-fn?-alias"
-	  (makunbound 'symbol?)
-	  (fn-p-to-fn?-alias 'symbolp)
-	  (assert-t (symbol? 'foo)
-		    "symbol? be a defined function")
+ 	  (makunbound 'symbol?)
+ 	  (fn-p-to-fn?-alias 'symbolp)
+ 	  (assert-t (symbol? 'foo)
+ 		    "symbol? be a defined function")
 
-	  (makunbound 'file-exists?)
-	  (fn-p-to-fn?-alias 'file-exists-p)
-	  (assert-t (file-exists? "./behave.el")
-		    "file-exists? should be a defined function")
+ 	  (makunbound 'file-exists?)
+ 	  (fn-p-to-fn?-alias 'file-exists-p)
+ 	  (assert-t (file-exists? "./behave.el")
+ 		    "file-exists? should be a defined function")
 
-	  (makunbound 'equal?)
-	  (fn-p-to-fn?-alias 'equal)
-	  (assert-t (equal? "a" "a")
-		    "equal? should be a defined function")
-	  ))
+ 	  (makunbound 'equal?)
+ 	  (fn-p-to-fn?-alias 'equal)
+ 	  (assert-t (equal? "a" "a")
+ 		    "equal? should be a defined function")
+ 	  )
+ 
+ (specify "buffer-killed? - nil"
+ 	  (assert-nil (buffer-killed? (current-buffer))))
+
+ (specify "buffer-killed? - t"
+ 	  (let ((buf (get-buffer-create
+ 		      (generate-new-buffer-name "*temp file test*"))))
+ 	    (kill-buffer buf)
+ 	    (assert-t (buffer-killed? buf))))
+ 
+ (specify "with-current-buffer-safe -t "
+ 	  (let ((buf (get-buffer-create
+ 		      (generate-new-buffer-name "*temp file test*"))))
+ 	    (assert-t
+ 	     (with-current-buffer-safe buf
+ 	       t))))
+ 
+ (specify "with-current-buffer-safe - killed"
+	  (let ((buf (get-buffer-create
+		      (generate-new-buffer-name "*temp file test*"))))
+	    (kill-buffer buf)
+	    (assert-nil
+	     (with-current-buffer-safe buf
+	       t))))
+ 
+)
 
 (behave "helper")
 
