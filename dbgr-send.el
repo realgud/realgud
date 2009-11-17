97d79fde73f6ac97db2f28a5118aafe4e5d18b75
R. Bernstein <rocky@gnu.org>
2009-11-17 12:49:33 -0500

    Don't issue message if we are in a command buffer Fill in test cases more.

--- dbgr-send.el
+++ dbgr-send.el
@@ -40,11 +40,14 @@ results into the command buffer."
 from the debugging session. OPT-NUM-STR is an optional number string.
 If present %-escapes in the string arguments are expanded. These are:
 
-  %f -- Name (without directory) of current source file.
-  %F -- Name (without directory or extension) of current source file.
+  %f -- Name without directory of current source file.
+  %F -- Name without directory or extension of current source file.
+  %x -- Name of current source file.
+  %X -- Expanded name of current source file.
   %d -- Directory of current source file.
   %l -- Number of current source line.
-  %p -- Prefix argument to the command (if any) as a number.
+  %p -- Numeric prefix argument converted to a string
+        If no prefix argument %p is the null string.
 "
   (let* ((buffer (or opt-buffer (current-buffer)))
 	 (srcbuf (dbgr-get-srcbuf buffer))
@@ -53,13 +56,17 @@ If present %-escapes in the string arguments are expanded. These are:
 	 result)
     (while (and fmt-str
 		(let ((case-fold-search nil))
-		  (string-match "\\([^%]*\\)%\\([dfFlp]\\)" fmt-str)))
+		  (string-match "\\([^%]*\\)%\\([dfFlpxX]\\)" fmt-str)))
       (let* ((key-str (match-string 2 fmt-str))
 	     (key (string-to-char key-str)))
 	(setq result 
 	      (concat 
 	       result (match-string 1 fmt-str) 
 	       (cond
+		((eq key ?d)
+		 (or (and src-file-name
+			  (file-name-directory src-file-name))
+		     "*source-file-not-found-for-%d"))
 		((eq key ?f)
 		 (or (and src-file-name
 			  (file-name-nondirectory src-file-name))
@@ -69,10 +76,6 @@ If present %-escapes in the string arguments are expanded. These are:
 			  (file-name-sans-extension
 			   (file-name-nondirectory src-file-name)))
 		     "*source-file-not-found-for-%F"))
-		((eq key ?d)
-		 (or (and src-file-name
-			  (file-name-directory src-file-name))
-		     "*source-file-not-found-for-%d"))
 		((eq key ?l)
 		 (if srcbuf
 		     (with-current-buffer srcbuf
@@ -82,6 +85,12 @@ If present %-escapes in the string arguments are expanded. These are:
 			  (+ (count-lines (point-min) (point))
 			     (if (bolp) 1 0)))))
 		   "source-buffer-not-found-for-%l"))
+		((eq key ?x)
+		 (or (and src-file-name src-file-name)
+		     "*source-file-not-found-for-%x"))
+		((eq key ?X)
+		 (or (and src-file-name (expand-file-name src-file-name))
+		     "*source-file-not-found-for-%X"))
 		;; ((eq key ?e)
 		;;  (gud-find-expr))
 		;; ((eq key ?a)
@@ -97,8 +106,10 @@ If present %-escapes in the string arguments are expanded. These are:
 (defun dbgr-command (fmt &optional arg)
   (interactive "sCommand (may contain format chars): ")
   (let ((command-str (dbgr-expand-format fmt arg)))
-    (message "Command: %s" command-str)
-    (sit-for 0)
+    (unless (dbgr-cmdbuf?)
+	(progn
+	  (message "Command: %s" command-str)
+	  (sit-for 0)))
     (dbgr-send-command command-str)))
 
 (provide 'dbgr-send)
--- test/test-send.el
+++ test/test-send.el
@@ -1,17 +1,54 @@
 (load-file "./behave.el")
 
 ;; FIXME: not sure why we have to include since dbgr-send includes this.
+(load-file "../dbgr-helper.el")
+(load-file "../dbgr-cmdbuf.el")
+(load-file "../dbgr-srcbuf.el")
 (load-file "../dbgr-buffer.el")
 
 (load-file "../dbgr-send.el")
 (behave-clear-contexts)
+
+(defvar temp-cmdbuf nil)
+(defun setup ()
+  (setq temp-cmdbuf (generate-new-buffer "*cmdbuf-test*"))
+  (dbgr-cmdbuf-init temp-cmdbuf "bogus-debugger")
+  (dbgr-srcbuf-init (current-buffer) temp-cmdbuf 
+		    "bugus-debugger" 
+		    '("/bin/bogus-debugger" "my-script" "arg1"))
+)
+
+(defun tear-down()
+  (kill-buffer temp-cmdbuf)
+)
 
 (context "dbgr-send"
 	 (tag send)
 	 (specify "format no expand characters"
 		  (dolist (str '("abc" "100%" "I feel %% today"))
 		    (assert-equal str (dbgr-expand-format str)))
-	   ))
+	   )
+	 (specify "format %l - with arg"
+		  (assert-equal "line 5" (dbgr-expand-format "line %p" 5)))
+	 (specify "format %l - without arg"
+		  (assert-equal "line " (dbgr-expand-format "line %p")))
+
+	 (setup)
+	 ;; Current buffer is now set up as a source buffer
+	 (setq file-name (buffer-file-name))
+	 (if file-name
+	     (specify "File formatting"
+		      (dolist 
+			  (pair 
+			   (list 
+			    (cons "%d" (file-name-directory file-name))
+			    (cons "%x" file-name)
+			    (cons "%X" (expand-file-name file-name))
+			    (cons "%f" "test-send.el")
+			    (cons "%F" "test-send")))
+			(assert-equal (cdr pair) (dbgr-expand-format (car pair))))))
+	 (tear-down)
+)
 
 (behave "send")
 
