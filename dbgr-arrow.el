2f3cae2c905782a629745d454e5047f84dbf5629
rocky <rockyb@rubyforge.org>
2009-11-19 07:11:51 -0500

    Fringe arrow cleanup and simplification in advance of reworking to add a source buffer-local variable of ring of history positions and marking them in the fringe in both the source and command buffers.
    list of positions

--- dbgr-arrow.el
+++ dbgr-arrow.el
@@ -41,7 +41,16 @@ position.")
 position.")
 
 (eval-when-compile (require 'cl))
+
 ;; Loop to set up fringe position markers. 
+
+;; Here is an example of what each iteration does:
+;;
+;;   (make-local-variable 'dbgr-overlay-arrow1) ;; or 2, or 3
+;;   (put 'dbgr-overlay-arrow1 'overlay-arrow-string "=>" ;; or "2>", or ">3"
+;;   (define-fringe-bitmap 'dbgr-overlay-arrow-face1 "\xc0...")
+;;   (add-to-list 'overlay-arrow-variable-list 'dbgr-overlay-arrow1)
+
 (dolist (pair 
 	 '( ("1" . "=>") ("2" . "2>") ("3" . "3>")))
   (let ((arrow-symbol (intern (concat "dbgr-overlay-arrow" (car pair))))
@@ -56,22 +65,15 @@ position.")
 	  (set-fringe-bitmap-face arrow-bitmap arrow-face)))
     (add-to-list 'overlay-arrow-variable-list arrow-symbol)))
 
-(defun dbgr-set-arrow (marker &optional opt-indicator-pair)
+(defun dbgr-set-arrow (marker &optional buffer)
   "Set the fringe indicator or overlay arrow to MARKER. This is done
 for example to indicate a debugger position."
-  (save-excursion
-    (let ((indicator-pair 
-	   (or opt-indicator-pair
-	   '((overlay-arrow . right-triangle)))))
-      (set-buffer (marker-buffer marker))
-      (setq overlay-arrow-position marker)
-      (setq overlay-arrow-string "=>")
-      (setq fringe-indicator-alist indicator-pair))))
+  (with-current-buffer (or buffer (current-buffer))
+    (setq dbgr-overlay-arrow1 marker)))
 
 (defun dbgr-unset-arrow (&optional buffer)
   "Remove fringe indicator or overlay arrow for BUFFER."
-  (save-excursion
-      (set-buffer (or buffer (current-buffer)))
-      (setq overlay-arrow-position nil)))
+  (with-current-buffer (or buffer (current-buffer))
+    (setq dbgr-overlay-arrow1 nil)))
 
 (provide 'dbgr-arrow)
