43d136d299f13ddb72cb601345c30849bc2011be
rocky <rocky@gnu.org>
2010-01-01 18:03:28 -0500

    Add breakpoint tracking for rdebug.

---
+++ test/test-regexp-rbdbgr.el
@@ -0,0 +1,54 @@
+(require 'test-unit)
+(load-file "../dbgr/init/rbdbgr.el")
+
+(test-unit-clear-contexts)
+
+
+(setq tb  (gethash "traceback" rbdbgr-pat-hash))
+(setq bps (gethash "brkpt-set" rbdbgr-pat-hash))
+
+(defun tb-loc-match(text) 
+  (string-match (dbgr-loc-pat-regexp tb) text)
+)
+
+(defun bp-loc-match(text) 
+  (string-match (dbgr-loc-pat-regexp bps) text)
+)
+
+;; FIXME: we get a void variable somewhere in here when running
+;;        even though we define it in lexical-let. Dunno why.
+;;        setq however will workaround this.
+(setq text "	from /usr/local/bin/irb:12:in `<main>'")
+(context "traceback location matching"
+	 (tag regexp-rbdbgr)
+	 (lexical-let ((text "	from /usr/local/bin/irb:12:in `<main>'"))
+	   (specify "basic traceback location"
+		    (assert-t (numberp (tb-loc-match text))))
+	   (specify "extract traceback file name"
+	   	    (assert-equal "/usr/local/bin/irb"
+				  (match-string (dbgr-loc-pat-file-group tb)
+	   				  text)))
+	   (specify "extract traceback line number"
+	   	    (assert-equal "12"
+				  (match-string (dbgr-loc-pat-line-group tb)
+						text)))
+	   )
+
+	 (lexical-let ((text "Breakpoint 1 set at line 9
+	in file /usr/local/bin/irb,
+	VM offset 2 of instruction sequence <top (required)>."))
+	   (specify "basic breakpoint location"
+		    (assert-t (numberp (bp-loc-match text))))
+	   (specify "extract breakpoint file name"
+	   	    (assert-equal "/usr/local/bin/irb"
+				  (match-string (dbgr-loc-pat-file-group bps)
+	   				  text)))
+	   (specify "extract breakpoint line number"
+	   	    (assert-equal "9"
+				  (match-string (dbgr-loc-pat-line-group bps)
+						text)))
+	   )
+	 )
+
+(test-unit "regexp-rbdbgr")
+
--- test/test-regexp-rdebug.el
+++ test/test-regexp-rdebug.el
@@ -1,11 +1,11 @@
 (require 'test-unit)
-(load-file "../dbgr/init/rbdbgr.el")
+(load-file "../dbgr/init/rdebug.el")
 
 (test-unit-clear-contexts)
 
 
-(setq tb  (gethash "traceback" rbdbgr-pat-hash))
-(setq bps (gethash "brkpt-set" rbdbgr-pat-hash))
+(setq tb  (gethash "traceback" rdebug-pat-hash))
+(setq bps (gethash "brkpt-set" rdebug-pat-hash))
 
 (defun tb-loc-match(text) 
   (string-match (dbgr-loc-pat-regexp tb) text)
@@ -20,7 +20,7 @@
 ;;        setq however will workaround this.
 (setq text "	from /usr/local/bin/irb:12:in `<main>'")
 (context "traceback location matching"
-	 (tag regexp-rbdbgr)
+	 (tag regexp-rdebug)
 	 (lexical-let ((text "	from /usr/local/bin/irb:12:in `<main>'"))
 	   (specify "basic traceback location"
 		    (assert-t (numberp (tb-loc-match text))))
@@ -34,21 +34,19 @@
 						text)))
 	   )
 
-	 (lexical-let ((text "Breakpoint 1 set at line 9
-	in file /usr/local/bin/irb,
-	VM offset 2 of instruction sequence <top (required)>."))
+	 (lexical-let ((text "Breakpoint 1 file /usr/bin/irb, line 10\n"))
 	   (specify "basic breakpoint location"
 		    (assert-t (numberp (bp-loc-match text))))
 	   (specify "extract breakpoint file name"
-	   	    (assert-equal "/usr/local/bin/irb"
+	   	    (assert-equal "/usr/bin/irb"
 				  (match-string (dbgr-loc-pat-file-group bps)
 	   				  text)))
-	   (specify "extract traceback line number"
-	   	    (assert-equal "9"
+	   (specify "extract breakpoint line number"
+	   	    (assert-equal "10"
 				  (match-string (dbgr-loc-pat-line-group bps)
 						text)))
 	   )
 	 )
 
-(test-unit "regexp-rbdbgr")
+(test-unit "regexp-rdebug")
 
