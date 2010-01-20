27bf28e5020dfc21e72526e5f65d1f60bc179a1b
rocky <rockyb@rubyforge.org>
2010-01-20 05:24:48 -0500

    Fill out more gdb interaction via dbgr.

---
+++ dbgr/gdb/gdb-track-mode.el
@@ -0,0 +1,49 @@
+;;; gdb tracking a comint or eshell buffer.
+
+(eval-when-compile (require 'cl))
+(require 'load-relative)
+(require-relative-list
+ '("../track-mode" "../cmds") "dbgr-")
+(require-relative "gdb-core")
+
+(defvar gdb-pat-hash)
+(defvar gdb-track-mode nil
+  "Non-nil if using gdb-track mode as a minor mode of some other mode.
+Use the command `dbgr-gdb-track-mode' to toggle or set this variable.")
+
+(declare-function dbgr-track-mode(bool))
+
+(defun dbgr-gdb-track-mode-body()
+  "Called when entering or leaving gdb-track-mode. Variable
+`gdb-track-mode' is a boolean which specifies if we are going
+into or out of this mode."
+  (dbgr-track-set-debugger "gdb")
+  (dbgr-define-gdb-like-commands)
+  (if gdb-track-mode
+      (progn 
+ 	;; FIXME: until I figure out why this isn't set in the mode
+        (local-set-key "\C-ce"  'gdb-goto-traceback-line)
+	(dbgr-track-mode 't)
+	(run-mode-hooks 'dbgr-gdb-track-mode-hook))
+    (progn 
+      (dbgr-track-mode nil)
+      (local-unset-key "\C-ce")
+    )))
+
+(defvar dbgr-gdb-track-mode-map
+  (let ((map (make-sparse-keymap)))
+    map)
+  "Keymap used in `gdb-track-mode'.")
+
+(define-minor-mode dbgr-gdb-track-mode
+  "Minor mode for tracking ruby debugging inside a process shell."
+  :init-value nil
+  ;; :lighter " gdb"   ;; mode-line indicator from dbgr-track is sufficient.
+  ;; The minor mode bindings.
+  :global nil
+  :group 'dbgr-gdb
+  :keymap dbgr-gdb-track-mode-map
+  (dbgr-gdb-track-mode-body)
+)
+
+(provide 'dbgr-gdb-track-mode)
---
+++ dbgr/gdb/gdb.el
@@ -0,0 +1,67 @@
+;;  `dbgr-gdb' Main interface to pydbgr via Emacs
+(require 'load-relative)
+(require-relative-list '("../helper") "dbgr-")
+(require-relative-list '("gdb-core" "gdb-track-mode"))
+
+;; This is needed, or at least the docstring part of it is needed to
+;; get the customization menu to work in Emacs 23.
+(defgroup dbgr-gdb nil
+  "The dbgr interface to gdb"
+  :group 'processes
+  :group 'tools
+  :version "23.1")
+
+;; -------------------------------------------------------------------
+;; User definable variables
+;;
+
+(defcustom dbgr-gdb-command-name
+  ;;"gdb --emacs 3"
+  "gdb"
+  "File name for executing the Ruby debugger and command options.
+This should be an executable on your path, or an absolute file name."
+  :type 'string
+  :group 'dbgr-gdb)
+
+(declare-function dbgr-gdb-track-mode (bool))
+
+;; -------------------------------------------------------------------
+;; The end.
+;;
+
+;;;###autoload
+(defun dbgr-gdb (&optional opt-command-line)
+  "Invoke the gdb Ruby debugger and start the Emacs user interface.
+
+String COMMAND-LINE specifies how to run gdb."
+  
+  (interactive)
+  (let* (
+	 (cmd-str (or opt-command-line (dbgr-gdb-query-cmdline "gdb")))
+	 (cmd-args (split-string-and-unquote cmd-str))
+	 (parsed-args (dbgr-gdb-parse-cmd-args cmd-args))
+	 (script-args (cdr cmd-args))
+	 (script-name (car script-args))
+	 (cmd-buf))
+  
+    ;; Parse the command line and pick out the script name and whether
+    ;; --annotate has been set.
+  
+    (condition-case nil
+	(setq cmd-buf 
+	      (apply 'dbgr-exec-shell "gdb" script-name
+		     (car cmd-args) (cdr cmd-args)))
+    (error nil))
+    ;; FIXME: Is there probably is a way to remove the
+    ;; below test and combine in condition-case? 
+    (let ((process (get-buffer-process cmd-buf)))
+      (if (and process (eq 'run (process-status process)))
+	  (progn
+	    (switch-to-buffer cmd-buf)
+	    (dbgr-gdb-track-mode 't)
+	    (dbgr-cmdbuf-info-cmd-args= dbgr-cmdbuf-info cmd-args)
+	    )
+	(message "Error running gdb command"))
+    )))
+
+(provide-me)
