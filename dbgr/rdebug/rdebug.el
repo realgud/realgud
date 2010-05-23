ef23e59608a7b578ae292a5bdd57b888713a5d23
rocky <rocky@gnu.org>
2010-05-23 02:56:49 +0200

    Add routine from rdebug to parse out command line options and get script name.

--- dbgr/rdebug/rdebug.el
+++ dbgr/rdebug/rdebug.el
@@ -29,6 +29,59 @@ This should be an executable on your path, or an absolute file name."
 ;; The end.
 ;;
 
+(defun rdebug-get-script-name (args)
+  "Parse command line ARGS.
+
+ARGS is a list of strings containing the rdebug command name. We
+return a list containing the script name, and whether the
+annotate option was set is returned.
+
+Initially annotate should be set to nil.  Argument ARGS contains
+a tokenized list of the command line."
+  ;; Parse the following:
+  ;;
+  ;;  [ruby ruby-options] rdebug rdebug-options script-name script-options
+  (and args
+       (let ((name nil)
+             (annotate-p nil))
+         ;; Strip of optional "ruby" or "ruby182" etc.
+         (when (string-match "^ruby[0-9]*$"
+                             (file-name-sans-extension
+                              (file-name-nondirectory (car args))))
+           (pop args)
+           (while (and args
+                       (string-match "^-" (car args)))
+             (if (member (car args) '("-e" "-r" "-I" "-C" "-F" "-K"))
+                 (pop args))
+             (pop args)))
+         ;; Remove "rdebug" from "rdebug --rdebug-options script
+         ;; --script-options"
+         (pop args)
+         ;; Skip to the first non-option argument.
+         (while (and args
+                     (not name))
+           (let ((arg (pop args)))
+             (cond
+              ;; Annotation or emacs option with level number.
+              ((or (member arg '("--annotate" "-A"))
+		   (equal arg "--emacs"))
+               (setq annotate-p t)
+               (pop args))
+              ;; Combined annotation and level option.
+              ((string-match "^--annotate=[0-9]" arg)
+               (setq annotate-p t))
+              ;; Options with arguments.
+              ((member arg '("-h" "--host" "-p" "--port"
+                             "-I" "--include" "-r" "--require"))
+               (pop args))
+              ((string-match "^-" arg)
+               nil)
+              (t
+               (setq name arg)))))
+         (and name
+              (list name annotate-p)))))
+
+
 ;;;###autoload
 (defun rdebug (&optional opt-command-line no-reset)
   "Invoke the rdebug Ruby debugger and start the Emacs user interface.
@@ -48,8 +101,10 @@ marginal icons is reset."
 	 (cmd-str (or opt-command-line (rdebug-query-cmdline "rdebug")))
 	 (cmd-args (split-string-and-unquote cmd-str))
 	 (parsed-args (rdebug-parse-cmd-args cmd-args))
+	 (script-name-annotate-p (rdebug-get-script-name cmd-args))
+	 (script-name (car script-name-annotate-p))
+	 (annotate-p  (cadr script-name-annotate-p))
 	 (script-args (cdr cmd-args))
-	 (script-name (car script-args))
 	 (cmd-buf))
   
     ;; Parse the command line and pick out the script name and whether
