58eff0710c0743ebc0b6382e48e9be49e4ec855f
rocky <rockyb@rubyforge.org>
2010-09-29 23:17:09 -0400

    DRY track mode code. Add M-kp-up and M-kp-down in standard keys.

---
+++ dbgr/pydbgr/cmds.el
@@ -0,0 +1,11 @@
+(require 'load-relative)
+(require-relative-list
+ '("../common/send") "dbgr-")
+
+(defun dbgr-define-pydbgr-commands ()
+  "(Re)define a bunch of pydbgr commands have. Right now nothing to do.."
+  )
+
+(provide-me "pydbgr-")
+
+
