6e7b42b2fa5b28ca1943fb6f20ef0a4dedfd3b3b
rocky <rocky@gnu.org>
2010-11-22 17:46:59 -0500

    Work on trepanx regexp test

--- test/test-regexp-trepanx.el
+++ test/test-regexp-trepanx.el
@@ -1,5 +1,6 @@
 (require 'test-unit)
 (load-file "../dbgr/debugger/trepanx/init.el")
+(load-file "../dbgr/lang/ruby.el")
 
 (test-unit-clear-contexts)
 
@@ -7,7 +8,6 @@
 (setq bps    (gethash "brkpt-set"     dbgr-trepanx-pat-hash))
 (setq prompt (gethash "prompt"        dbgr-trepanx-pat-hash))
 (setq tb     (gethash "backtrace"     dbgr-trepanx-pat-hash))
-(setq ctrl   (gethash "control-frame" dbgr-trepanx-pat-hash))
 
 (defun tb-loc-match(text) 
   (string-match (dbgr-loc-pat-regexp tb) text)
@@ -17,70 +17,58 @@
   (string-match (dbgr-loc-pat-regexp bps) text)
 )
 
-(defun ctrl-frame-match(text) 
-  (string-match (dbgr-loc-pat-regexp ctrl) text)
-)
-
 ;; FIXME: we get a void variable somewhere in here when running
 ;;        even though we define it in lexical-let. Dunno why.
 ;;        setq however will workaround this.
-(setq text "	from /usr/local/bin/irb:12:in `<main>'")
+(setq text" [0;31m                       Object#boom at tmp/boom.rb:2[0m")
+
 (context "traceback location matching"
 	 (tag regexp-trepanx)
-	 (lexical-let ((text "	from /usr/local/bin/irb:12:in `<main>'"))
-	   (specify "basic traceback location"
-		    (assert-t (numberp (tb-loc-match text))))
-	   (specify "extract traceback file name"
-	   	    (assert-equal "/usr/local/bin/irb"
-				  (match-string (dbgr-loc-pat-file-group tb)
-	   				  text)))
-	   (specify "extract traceback line number"
-	   	    (assert-equal "12"
-				  (match-string (dbgr-loc-pat-line-group tb)
-						text)))
-	   )
-
-	   (specify "prompt"
-	   	    (assert-equal 0 (string-match (dbgr-loc-pat-regexp prompt)
-						  "(trepanx): "))
-	   	    (assert-equal 0 (string-match (dbgr-loc-pat-regexp prompt)
-						  "((trepanx)): "))
-	   	    (assert-equal 0 (string-match (dbgr-loc-pat-regexp prompt)
-						  "((trepanx@55)): "))
-	   	    (assert-equal 0 (string-match (dbgr-loc-pat-regexp prompt)
-						  "((trepanx@main)): "))
-		    )
-
-	   (specify "control-frame"
-	   	    (assert-equal 0 (ctrl-frame-match 
-				     "c:0026 p:0181 s:0136 b:0136 l:000135 d:000135 METHOD /trepanx-0.0.1/app/frame.rb:132 "
-				     )
-				  )
-	   	    (assert-equal 0 (ctrl-frame-match 
-				     "c:0030 p:0041 s:0144 b:0144 l:00226c d:00226c METHOD /gems/trepanx-0.0.1/processor/eval.rb:15 "
-				     )
-				  )
-	   	    (assert-equal 0 (ctrl-frame-match 
-				     "c:0015 p:0139 s:0070 b:0070 l:000063 d:000069 BLOCK  /gems/app/core.rb:121"
-				     )
-				  )
-		    )
+	 (specify "basic traceback location"
+		  (assert-t (numberp (tb-loc-match text))))
+	 (specify "extract traceback file name"
+		  (assert-equal 0 (tb-loc-match text))
+		  (assert-equal "tmp/boom.rb"
+				(match-string (dbgr-loc-pat-file-group tb)
+					      text))
+		  (setq text 
+			"            { } in main.__script__ at /tmp/blam.rb:5")
+		  (assert-equal 0 (tb-loc-match text))
+		  (assert-equal "/tmp/blam.rb"
+				(match-string (dbgr-loc-pat-file-group tb)
+					      text))
+	 (specify "extract traceback line number"
+		  (assert-equal "5"
+				(match-string (dbgr-loc-pat-line-group tb)
+					      text)))
+	 
+	;;  (specify "prompt"
+	;; 	  (assert-equal 0 (string-match (dbgr-loc-pat-regexp prompt)
+	;; 					"(trepanx): "))
+	;; 	  (assert-equal 0 (string-match (dbgr-loc-pat-regexp prompt)
+	;; 					"((trepanx)): "))
+	;; 	  (assert-equal 0 (string-match (dbgr-loc-pat-regexp prompt)
+	;; 					"((trepanx@55)): "))
+	;; 	  (assert-equal 0 (string-match (dbgr-loc-pat-regexp prompt)
+	;; 					  "((trepanx@main)): "))
+	;; 	    )
 	   
-	   (lexical-let ((text "Breakpoint 1 set at line 9
-	in file /usr/local/bin/irb,
-	VM offset 2 of instruction sequence <top (required)>."))
-	     (specify "basic breakpoint location"
-		      (assert-t (numberp (bp-loc-match text))))
-	     (specify "extract breakpoint file name"
-		      (assert-equal "/usr/local/bin/irb"
-				    (match-string (dbgr-loc-pat-file-group bps)
-						  text)))
-	     (specify "extract breakpoint line number"
-		      (assert-equal "9"
-				    (match-string (dbgr-loc-pat-line-group bps)
-						  text)))
-	     )
-	   )
+	;;    (lexical-let ((text "Breakpoint 1 set at line 9
+	;; in file /usr/local/bin/irb,
+	;; VM offset 2 of instruction sequence <top (required)>."))
+	;;      (specify "basic breakpoint location"
+	;; 	      (assert-t (numberp (bp-loc-match text))))
+	;;      (specify "extract breakpoint file name"
+	;; 	      (assert-equal "/usr/local/bin/irb"
+	;; 			    (match-string (dbgr-loc-pat-file-group bps)
+	;; 					  text)))
+	;;      (specify "extract breakpoint line number"
+	;; 	      (assert-equal "9"
+	;; 			    (match-string (dbgr-loc-pat-line-group bps)
+	;; 					  text)))
+	;;      )
+	 )
+	 )
 
 (test-unit "regexp-trepanx")
 
