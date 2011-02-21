90fc4b35bc30d719bff857a16ff6212ea7f74b8b
rocky <rocky@gnu.org>
2011-02-21 08:02:42 -0500

    backtrack-mode: like track-mode but there doesn't have to be an associated process. This could be used for log files or processes that have terminated.

---
+++ dbgr/debugger/trepan/backtrack-mode.el
@@ -0,0 +1,59 @@
+;;; Copyright (C) 2011 Rocky Bernstein <rocky@gnu.org>
+
+;;; Mode for parsing various kinds of backtraces found in the Ruby
+
+(eval-when-compile (require 'cl))
+(require 'load-relative)
+(require-relative-list '(
+			 "../../common/cmds" 
+			 "../../common/menu"
+			 "../../common/backtrack-mode"
+			 ) 
+		       "dbgr-")
+(require-relative-list '("core" "cmds" "init") "dbgr-trepan-")
+(require-relative-list '("../../lang/ruby") "dbgr-lang-")
+
+(dbgr-backtrack-mode-vars "trepan")
+(set-keymap-parent trepan-backtrack-mode-map dbgr-backtrack-mode-map)
+
+(declare-function dbgr-backtrack-mode(bool))
+
+(defun dbgr-trepan-goto-control-frame-line (pt)
+  "Display the location mentioned by a control-frame line
+described by PT."
+  (interactive "d")
+  (dbgr-goto-line-for-pt pt "control-frame"))
+
+(dbgr-ruby-populate-command-keys trepan-backtrack-mode-map)
+(define-key trepan-backtrack-mode-map 
+  (kbd "C-c !c") 'dbgr-trepan-goto-control-frame-line)
+
+(define-minor-mode trepan-backtrack-mode
+  "Minor mode for tracking ruby debugging inside a file which may not have process shell."
+  :init-value nil
+  ;; :lighter " trepan"   ;; mode-line indicator from dbgr-track is sufficient.
+  ;; The minor mode bindings.
+  :global nil
+  :group 'trepan
+  :keymap trepan-backtrack-mode-map
+
+  (dbgr-backtrack-set-debugger "trepan")
+  (if trepan-backtrack-mode
+      (progn 
+	(dbgr-backtrack-mode 't)
+	(run-mode-hooks (intern (trepan-backtrack-mode-hook))))
+    (progn 
+      (dbgr-backtrack-mode nil)
+      ))
+)
+
+(defun trepan-backtrack-mode-hook()
+  (if trepan-backtrack-mode
+      (progn
+	(use-local-map trepan-backtrack-mode-map)
+	(message "using trepan mode map")
+	)
+    (message "trepan backtrack-mode-hook disable called"))
+)
+
+(provide-me "dbgr-trepan-")
--- test/test-loc-regexp-trepan.el
+++ test/test-loc-regexp-trepan.el
@@ -24,9 +24,9 @@
 
 (context "location matching"
 	 (tag loc-regexp-trepan)
-	 (setq text "-- (/usr/local/bin/irb:9 @2)")
 
 	 (specify "basic location"
+		  (setq text "-- (/usr/local/bin/irb:9 @2)")
 		  (assert-t (numberp (loc-match text)))
 		  )
 	 (specify "extract file name"
@@ -35,13 +35,37 @@
 				(match-string (dbgr-cmdbuf-info-file-group dbgr)
 						text))
 		  )
-	   (specify "extract line number"
-	   	    (assert-equal "9"
-				  (match-string 
-				   (dbgr-cmdbuf-info-line-group dbgr)
-				   text))
-		    )
-	   )
+	 (specify "extract line number"
+		  (assert-equal "9"
+				(match-string 
+				 (dbgr-cmdbuf-info-line-group dbgr)
+				 text))
+		  )
+
+	 )
+
+
+(context "location matching c-func"
+	 (tag loc-regexp-trepan)
+
+	 (specify "basic location for C fn"
+		  (setq text "C> (/tmp/c-func.rb:2)")
+		  (assert-t (numberp (loc-match text)))
+		  )
+	 (specify "extract file name for C fn"
+		  (assert-equal 0 (loc-match text))
+		  (assert-equal "/tmp/c-func.rb"
+				(match-string (dbgr-cmdbuf-info-file-group dbgr)
+						text))
+		  )
+	 (specify "extract line number for C fn"
+		  (assert-equal "2"
+				(match-string 
+				 (dbgr-cmdbuf-info-line-group dbgr)
+				 text))
+		  )
+	 )
+
 
 (test-unit "loc-regexp-trepan")
 
