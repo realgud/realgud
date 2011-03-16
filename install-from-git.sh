e8702c70894639ef2439eec10f837b9e75b6a2a5
rocky <rocky@gnu.org>
2011-03-16 13:20:03 -0400

    Add program to install from git

---
+++ install-from-git.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Install emacs-dbgr from git
+run_cmd() {
+    echo "--- Running command: $@"
+    $@
+    rc=$?
+    echo "--- $@ exit status is $?"
+    return $rc
+}
+
+if (( $(id -u) != 0)) ; then 
+    need_sudo='sudo'
+else
+    need_sudo=''
+fi
+for program in git make $need_sudo ; do
+    if ! which $program >/dev/null 2>&1 ; then
+	echo "Cant find program $program in $PATH"
+	exit 1
+    fi
+done
+
+# for pkg in emacs-{test-unit,load-relative,loc-changes,dbgr} ; do 
+for pkg in emacs-{test-unit,load-relative} ; do 
+    echo '******************************************'
+    echo Trying to install ${pkg}...
+    echo '******************************************'
+    run_cmd git clone http://github.com/rocky/${pkg}.git
+    (cd $pkg && \
+        run_cmd $SHELL ./autogen.sh && \
+	run_cmd ./configure && \
+	run_cmd make && \
+	run_cmd make check && \
+        run_cmd $need_sudo make install
+    )
+done
