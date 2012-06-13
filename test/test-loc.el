c8ea5ee1599bb8e92ca02ad83e8b5d0fb4f838ce
rocky <rocky@gnu.org>
2012-06-13 05:35:55 -0400

    test-unit -> test-simple

--- test/test-loc.el
+++ test/test-loc.el
@@ -1,49 +1,50 @@
-(require 'test-unit)
+(require 'test-simple)
 (load-file "../dbgr/common/loc.el")
 
-(test-unit-clear-contexts)
+(test-simple-start)
 
 (save-current-buffer
 
   ;; Below, we need to make sure current-buffer has an associated
   ;; file with it.
-  (find-file (symbol-file 'test-unit))
+  (find-file (symbol-file 'test-simple))
 
-  (context "location field extraction"
-	   (tag loc)
-	   (lexical-let* ((buff (current-buffer))
-			  (filename (buffer-file-name buff))
-			  (source-marker (point-marker))
-			  (cmd-marker (point-marker))
-			  (good-loc (make-dbgr-loc 
-				     :filename filename 
-				     :line-number 5 
-				     :marker source-marker
-				     :cmd-marker cmd-marker
-				     ))
-			  (good-loc2 (make-dbgr-loc 
-				      :filename filename 
-				      :line-number 6
-				     :marker source-marker
-				     :cmd-marker cmd-marker
-				     ))
-			  (good-loc3 (dbgr-loc-current buff cmd-marker)))
-	     
-	     (specify "line-number extraction"
-		      (assert-equal 5 (dbgr-loc-line-number good-loc)))
-	     (specify "source code marker extraction"
-		      (assert-equal source-marker (dbgr-loc-marker good-loc)))
+  (note "location field extraction")
+  (let* ((buff (current-buffer))
+	 (filename (buffer-file-name buff))
+	 (source-marker (point-marker))
+	 (cmd-marker (point-marker))
+	 (good-loc (make-dbgr-loc 
+		    :filename filename 
+		    :line-number 5 
+		    :marker source-marker
+		    :cmd-marker cmd-marker
+		    ))
+	 (good-loc2 (make-dbgr-loc 
+		     :filename filename 
+		     :line-number 6
+		     :marker source-marker
+		     :cmd-marker cmd-marker
+		     ))
+	 (good-loc3 (dbgr-loc-current buff cmd-marker)))
+    
+    (assert-equal 5 (dbgr-loc-line-number good-loc) "line-number extraction")
+    
+    (assert-equal source-marker (dbgr-loc-marker good-loc) 
+		  "source code marker extraction")
 
-	     (specify "command process marker extraction"
-		      (assert-equal cmd-marker (dbgr-loc-cmd-marker good-loc)))
+    
+    (assert-equal cmd-marker (dbgr-loc-cmd-marker good-loc)
+		  "command process marker extraction")
 
-	     (specify "marker set"
-		      (dbgr-loc-marker= good-loc2 source-marker)
-		      (assert-equal source-marker (dbgr-loc-marker good-loc2)))
+    
+    (dbgr-loc-marker= good-loc2 source-marker)
+    (assert-equal source-marker (dbgr-loc-marker good-loc2)
+		  "marker set")
 
-	     )))
+    ))
 
-(test-unit "loc")
+(end-tests)
 
 ; TODO: add test for debug-loc-goto, e.g.
 ;(dbgr-loc-goto (dbgr-loc-new "/tmp/bashdb.diff" 8))
