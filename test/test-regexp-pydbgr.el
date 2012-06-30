64e7f883f7da6adc0ccfe955344a47585d4ee555
rocky <rocky@gnu.org>
2012-07-01 03:24:17 +0700

    test-unit -> test-simple

--- test/test-regexp-pydbgr.el
+++ test/test-regexp-pydbgr.el
@@ -1,8 +1,7 @@
-(require 'test-unit)
+(require 'test-simple)
 (load-file "../dbgr/debugger/pydbgr/init.el")
 
-(test-unit-clear-contexts)
-
+(test-simple-start)
 
 (setq bps    (gethash "brkpt-set" dbgr-pydbgr-pat-hash))
 (setq loc    (gethash "loc"       dbgr-pydbgr-pat-hash))
@@ -16,71 +15,53 @@
 ;;        even though we define it in lexical-let. Dunno why.
 ;;        setq however will workaround this.
 (setq text "  File \"/usr/lib/python2.6/code.py\", line 281, in raw_input")
-(context "traceback location matching"
-	 (tag regexp-pydbgr)
-	 (specify "basic traceback location"
-		  (assert-t (numberp (loc-match text tb))))
-	 (specify "extract file name"
-		  (assert-equal "/usr/lib/python2.6/code.py"
-				(match-string (dbgr-loc-pat-file-group tb)
-					      text)
-				(format "Failing file group is %s" 
-					(dbgr-loc-pat-file-group tb))))
-	 (specify "extract line number"
-		  (assert-equal "281"
-				(match-string (dbgr-loc-pat-line-group tb)
-					      text))
-		  ))
 
-(context "breakpoint location matching"
-	 (tag regexp-pydbgr)
-	 (lexical-let ((text "Breakpoint 1 set at line 13 of file /src/git/code/gcd.py"))
-	   (specify "basic breakpoint location"
-		    (assert-t (numberp (loc-match text bps))))
-	   (specify "extract breakpoint file name"
-	   	    (assert-equal "/src/git/code/gcd.py"
-				  (match-string (dbgr-loc-pat-file-group bps)
-	   				  text)))
-	   (specify "extract breakpoint line number"
-	   	    (assert-equal "13"
-				  (match-string (dbgr-loc-pat-line-group bps)
-						text)))
-	   )
-	 )
+(note "traceback location matching")
+(assert-t (numberp (loc-match text tb)) "basic traceback location")
+(assert-equal "/usr/lib/python2.6/code.py"
+	      (match-string (dbgr-loc-pat-file-group tb)
+			    text)
+	      (format "Failing file group is %s" 
+		      (dbgr-loc-pat-file-group tb) "extract file name"))
+(assert-equal "281"
+	      (match-string (dbgr-loc-pat-line-group tb)
+			    text) "extract line number")
 
-(context "prompt matching"
-	 (tag regexp-pydbgr)
-	 (lexical-let ((text "(c:\\working\\python\\helloworld.py:30): <module>"))
-	   (specify "MS DOS position location"
-		    (assert-t (numberp (loc-match text loc))))
-	   (specify "extract file name"
-		    (assert-equal "c:\\working\\python\\helloworld.py"
-				(match-string (dbgr-loc-pat-file-group loc)
-					      text)
-				(format "Failing file group is %s" 
-					(dbgr-loc-pat-file-group tb))))
-	 (specify "extract line number"
-		  (assert-equal "30"
-				(match-string (dbgr-loc-pat-line-group loc)
-					      text)))
+(note "breakpoint location matching")
+(lexical-let ((text "Breakpoint 1 set at line 13 of file /src/git/code/gcd.py"))
+  (assert-t (numberp (loc-match text bps))  "basic breakpoint location")
+  (assert-equal "/src/git/code/gcd.py"
+		(match-string (dbgr-loc-pat-file-group bps)
+			      text)   "extract breakpoint file name")
+  (assert-equal "13"
+		(match-string (dbgr-loc-pat-line-group bps)
+			      text)   "extract breakpoint line number")
+  )
 
-	   )
-	 (lexical-let ((text "(/usr/bin/ipython:24): <module>"))
-	   (specify "position location"
-		    (assert-t (numberp (loc-match text loc))))
-	   (specify "extract file name"
-		    (assert-equal "/usr/bin/ipython"
-				(match-string (dbgr-loc-pat-file-group loc)
-					      text)
-				(format "Failing file group is %s" 
-					(dbgr-loc-pat-file-group tb))))
-	 (specify "extract line number"
-		  (assert-equal "24"
-				(match-string (dbgr-loc-pat-line-group loc)
-					      text)))
+(note "prompt matching")
+(lexical-let ((text "(c:\\working\\python\\helloworld.py:30): <module>"))
+  (assert-t (numberp (loc-match text loc))   "MS DOS position location")
+  (assert-equal "c:\\working\\python\\helloworld.py"
+		(match-string (dbgr-loc-pat-file-group loc)
+			      text)
+		(format "Failing file group is %s" 
+			(dbgr-loc-pat-file-group tb) "extract file name"))
+  (assert-equal "30"
+		(match-string (dbgr-loc-pat-line-group loc)
+			      text)   "extract line number")
 
-	   )
-	 )
+  )
+(lexical-let ((text "(/usr/bin/ipython:24): <module>"))
+  (assert-t (numberp (loc-match text loc))   "position location")
+  (assert-equal "/usr/bin/ipython"
+		(match-string (dbgr-loc-pat-file-group loc)
+			      text)
+		(format "Failing file group is %s" 
+			(dbgr-loc-pat-file-group tb) "extract file name"))
+  (assert-equal "24"
+		(match-string (dbgr-loc-pat-line-group loc)
+			      text)   "extract line number")
+  )
 
-(test-unit "regexp-pydbgr")
+(end-tests)
 
--- test/test-regexp-ruby.el
+++ test/test-regexp-ruby.el
@@ -1,10 +1,10 @@
-(require 'test-unit)
+(require 'test-simple)
 
 ;; Don't have a pat-hash for ruby, so we need something that pulls in
 ;; Ruby.
 (load-file "../dbgr/debugger/rdebug/init.el")
 
-(test-unit-clear-contexts)
+(test-simple-start)
 
 (setq bt  (gethash "rails-backtrace" dbgr-rdebug-pat-hash))
 
@@ -16,34 +16,29 @@
 ;;        even though we define it in lexical-let. Dunno why.
 ;;        setq however will workaround this.
 (setq text "/tmp/rails-2.3.5/lib/tasks/databases.rake:360")
-(context "traceback location matching"
-	 (tag regexp-ruby)
-	 (lexical-let ((text "/tmp/rails-2.3.5/lib/tasks/databases.rake:360"))
-	   (specify "basic traceback location"
-		    (assert-t (numberp (loc-match text bt))))
-	   (specify "extract traceback file name"
-		    (assert-equal "/tmp/rails-2.3.5/lib/tasks/databases.rake"
-				  (match-string (dbgr-loc-pat-file-group bt)
-						text)))
-	   (specify "extract traceback line number"
-		    (assert-equal "360"
-				  (match-string (dbgr-loc-pat-line-group bt)
-					      text)))
-	   )
 
-	 (lexical-let ((text 
-			"/tmp/gems/rake-0.8.7/lib/rake.rb:597:in `invoke_with_call_chain'"))
-	   (specify "traceback location with in"
-		    (assert-t (numberp (loc-match text bt))))
-	   (specify "extract traceback file name 2"
-	   	  (assert-equal "/tmp/gems/rake-0.8.7/lib/rake.rb"
-	   			(match-string (dbgr-loc-pat-file-group bt)
-	   				      text)))
-	   (specify "extract breakpoint line number 2"
-		    (assert-equal "597"
-				  (match-string (dbgr-loc-pat-line-group bt)
-						text)))
-	   )
-	 )
-(test-unit "regexp-ruby")
+(lexical-let ((text "/tmp/rails-2.3.5/lib/tasks/databases.rake:360"))
+  
+  (assert-t (numberp (loc-match text bt)) "basic traceback location")
+  (assert-equal "/tmp/rails-2.3.5/lib/tasks/databases.rake"
+		(match-string (dbgr-loc-pat-file-group bt)
+			      text) "extract traceback file name")
+  (assert-equal "360"
+		(match-string (dbgr-loc-pat-line-group bt)
+			      text)   "extract traceback line number")
+  )
+
+(lexical-let ((text 
+	       "/tmp/gems/rake-0.8.7/lib/rake.rb:597:in `invoke_with_call_chain'"))
+  
+  (assert-t (numberp (loc-match text bt)) "traceback location with in")
+  (assert-equal "/tmp/gems/rake-0.8.7/lib/rake.rb"
+		(match-string (dbgr-loc-pat-file-group bt)
+			      text)   "extract traceback file name 2")
+  (assert-equal "597"
+		(match-string (dbgr-loc-pat-line-group bt)
+			      text)   "extract breakpoint line number 2")
+  )
+
+(end-tests)
 
