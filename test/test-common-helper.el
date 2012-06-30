4c3f892a9832a048500a661a174bae491e210b83
rocky <rocky@gnu.org>
2012-07-01 03:38:01 +0700

    test-unit -> test-simple

--- test/test-common-helper.el
+++ test/test-common-helper.el
@@ -1,22 +1,16 @@
-(require 'test-unit)
+(require 'test-simple)
 (load-file "../dbgr/common/helper.el")
 
-(test-unit-clear-contexts)
+(test-simple-start)
 
 (defstruct dbgr-test-info name)
 (dbgr-struct-field-setter "dbgr-test-info" "name")
 
-(context "common-helper"
-	 (tag common-helper)
-	 
-	 (set (make-local-variable 'dbgr-test-info)
-	      (make-dbgr-test-info :name "foo"))
-	 
-	 (specify "setter macro works"
-		  (assert-t (functionp 'dbgr-test-info-name=))
-		  (assert-equal "foo" (dbgr-test-info-name= "foo")
-				)
-		  )
-	 )
+(set (make-local-variable 'dbgr-test-info)
+     (make-dbgr-test-info :name "foo"))
 
-(test-unit "common-helper")
+(note "setter macro works")
+(assert-t (functionp 'dbgr-test-info-name=))
+(assert-equal "foo" (dbgr-test-info-name= "foo"))
+
+(end-tests)
--- test/test-remake-core.el
+++ test/test-remake-core.el
@@ -1,36 +1,29 @@
-(require 'test-unit)
+(require 'test-simple)
 (load-file "../dbgr/debugger/remake/core.el")
 
-(test-unit-clear-contexts)
+(test-simple-start)
 
-(context "remake-core"
-	 (tag remake-core)
+(assert-equal '("remake" "Makefile" ("-X" "-f" "Makefile"))
+	      (remake-parse-cmd-args 
+	       '("remake" "-X" "-f" "Makefile")) "remake-parse-cmd-args")
 
-	 (specify "remake-parse-cmd-args"
-	      (assert-equal '("remake" "Makefile" ("-X" "-f" "Makefile"))
-			    (remake-parse-cmd-args 
-			     '("remake" "-X" "-f" "Makefile")))
-	      )
-	 (specify "remake-suggest-Makefile"
-	      (assert-equal "Makefile" (remake-suggest-Makefile))
-	      )
+(assert-equal "Makefile" (remake-suggest-Makefile) "remake-suggest-Makefile")
 
-	 (specify "remake-file-suggest-priority"
-	      (assert-equal 2 (remake-suggest-file-priority "foo"))
-	      (let ((buffer (get-file-buffer "Makefile.am")))
-	 	(if buffer (kill-buffer buffer))
-	 	(assert-equal 2 (remake-suggest-file-priority "Makefile.am"))
-	 	(setq buffer (find-file-noselect "Makefile.am"))
-	 	(assert-equal 5 (remake-suggest-file-priority "Makefile.am"))
-	 	(kill-buffer buffer)
-	 	(setq buffer (get-file-buffer "Makefile"))
-	 	(if buffer (kill-buffer buffer))
-	 	(assert-equal 6 (remake-suggest-file-priority "Makefile"))
-	 	(setq buffer (find-file-noselect "Makefile"))
-	 	(assert-equal 8 (remake-suggest-file-priority "Makefile"))
-	 	(kill-buffer buffer)
-	      ))
-	 )
+(assert-equal 2 (remake-suggest-file-priority "foo") 
+	      "remake-file-suggest-priority")
+(let ((buffer (get-file-buffer "Makefile.am")))
+  (if buffer (kill-buffer buffer))
+  (assert-equal 2 (remake-suggest-file-priority "Makefile.am"))
+  (setq buffer (find-file-noselect "Makefile.am"))
+  (assert-equal 5 (remake-suggest-file-priority "Makefile.am"))
+  (kill-buffer buffer)
+  (setq buffer (get-file-buffer "Makefile"))
+  (if buffer (kill-buffer buffer))
+  (assert-equal 6 (remake-suggest-file-priority "Makefile"))
+  (setq buffer (find-file-noselect "Makefile"))
+  (assert-equal 8 (remake-suggest-file-priority "Makefile"))
+  (kill-buffer buffer)
+  )
 
-(test-unit "remake-core")
+(end-tests)
 
