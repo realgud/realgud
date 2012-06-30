cf4f367aed0e3670a534140403e7ae9644eeb768
rocky <rocky@gnu.org>
2012-07-01 02:56:41 +0700

    test-unit -> test-simple

--- test/test-regexp-gdb.el
+++ test/test-regexp-gdb.el
@@ -1,9 +1,8 @@
-(require 'test-unit)
+(require 'test-simple)
 (load-file "../dbgr/common/buffer/command.el")
 (load-file "../dbgr/debugger/gdb/init.el")
 
-(test-unit-clear-contexts)
-
+(test-simple-start)
 
 ; Some setup usually done in setting up the buffer.
 ; We customize this for this debugger.
@@ -21,83 +20,78 @@
 ;;        even though we define it in lexical-let. Dunno why.
 ;;        setq however will workaround this.
 (setq text "/home/rocky/c/ctest.c:80:2000:beg:0x8048748>")
-(context "traceback location matching"
-	 (tag regexp-gdb)
-	 (defun loc-match(text) 
-	   (string-match (dbgr-cmdbuf-info-loc-regexp dbgr) text)
-	   )
-	 (specify "basic location"
-		  (assert-t (numberp (loc-match text))))
-	 (specify "extract file name"
-		  (assert-equal "/home/rocky/c/ctest.c"
-				(match-string (dbgr-cmdbuf-info-file-group dbgr)
-					      text)))
-	 
-	 (specify "extract line number"
-		  (assert-equal "80"
-				(match-string (dbgr-cmdbuf-info-line-group dbgr)
-					      text)))
-	 (specify "debugger-backtrace"
-		  (setq dbgr-bt-pat  (gethash "debugger-backtrace"  
-					     dbgr-gdb-pat-hash))
-		  (setq s1
-			"#0  main (argc=2, argv=0xbffff564, envp=0xbffff570) at main.c:935
+(note "traceback location matching")
+
+(defun loc-match(text) 
+  (string-match (dbgr-cmdbuf-info-loc-regexp dbgr) text)
+  )
+
+(assert-t (numberp (loc-match text)) "basic location")
+(assert-equal "/home/rocky/c/ctest.c"
+	      (match-string (dbgr-cmdbuf-info-file-group dbgr)
+			    text) "extract file name")
+(assert-equal "80"
+	      (match-string (dbgr-cmdbuf-info-line-group dbgr)
+			    text) "extract line number")
+(note "debugger-backtrace")
+(setq dbgr-bt-pat  (gethash "debugger-backtrace"  
+			    dbgr-gdb-pat-hash))
+(setq s1
+      "#0  main (argc=2, argv=0xbffff564, envp=0xbffff570) at main.c:935
 #1  0xb7e9f4a5 in *__GI___strdup (s=0xbffff760 \"/tmp/remake/remake\") at strdup.c:42
 #2  0x080593ac in main (argc=2, argv=0xbffff5a4, envp=0xbffff5b0)
     at main.c:952
 #46 0xb7f51b87 in vm_call_cfunc (th=0x804d188, reg_cfp=0xb7ba9e88, num=0, 
     recv=157798080, blockptr=0x0, me=0x80d12a0) at vm_insnhelper.c:410
 ")
-		  (setq dbgr-bt-re (dbgr-loc-pat-regexp dbgr-bt-pat))
-		  (setq file-group (dbgr-loc-pat-file-group dbgr-bt-pat))
-		  (setq line-group (dbgr-loc-pat-line-group dbgr-bt-pat))
-		  (assert-equal 0 (string-match dbgr-bt-re s1))
-		  (assert-equal "main.c"
-				(substring s1 
-					   (match-beginning file-group)
-					   (match-end file-group)))
-		  (assert-equal "935"
-				(substring s1 
-					   (match-beginning line-group)
-					   (match-end line-group)))
+(setq dbgr-bt-re (dbgr-loc-pat-regexp dbgr-bt-pat))
+(setq file-group (dbgr-loc-pat-file-group dbgr-bt-pat))
+(setq line-group (dbgr-loc-pat-line-group dbgr-bt-pat))
+(assert-equal 0 (string-match dbgr-bt-re s1))
+(assert-equal "main.c"
+	      (substring s1 
+			 (match-beginning file-group)
+			 (match-end file-group)))
+(assert-equal "935"
+	      (substring s1 
+			 (match-beginning line-group)
+			 (match-end line-group)))
 
-		  (setq pos (match-end 0))
-		  (assert-equal 65 pos)
-		  (assert-equal 65 (string-match dbgr-bt-re s1 pos))
-		  (assert-equal "strdup.c"
-				(substring s1 
-					   (match-beginning file-group)
-					   (match-end file-group)))
-		  (assert-equal "42"
-				(substring s1 
-					   (match-beginning line-group)
-					   (match-end line-group)))
+(setq pos (match-end 0))
+(assert-equal 65 pos)
+(assert-equal 65 (string-match dbgr-bt-re s1 pos))
+(assert-equal "strdup.c"
+	      (substring s1 
+			 (match-beginning file-group)
+			 (match-end file-group)))
+(assert-equal "42"
+	      (substring s1 
+			 (match-beginning line-group)
+			 (match-end line-group)))
 
-		  (setq pos (match-end 0))
-		  (assert-equal 149 pos)
-		  (assert-equal 149 (string-match dbgr-bt-re s1 pos))
-		  (assert-equal "main.c"
-				(substring s1 
-					   (match-beginning file-group)
-					   (match-end file-group)))
-		  (assert-equal "952"
-				(substring s1 
-					   (match-beginning line-group)
-					   (match-end line-group)))
+(setq pos (match-end 0))
+(assert-equal 149 pos)
+(assert-equal 149 (string-match dbgr-bt-re s1 pos))
+(assert-equal "main.c"
+	      (substring s1 
+			 (match-beginning file-group)
+			 (match-end file-group)))
+(assert-equal "952"
+	      (substring s1 
+			 (match-beginning line-group)
+			 (match-end line-group)))
 
-		  (setq pos (match-end 0))
-		  (assert-equal 233 pos)
-		  (assert-equal 233 (string-match dbgr-bt-re s1 pos))
-		  (assert-equal "vm_insnhelper.c"
-				(substring s1 
-					   (match-beginning file-group)
-					   (match-end file-group)))
-		  (assert-equal "410"
-				(substring s1 
-					   (match-beginning line-group)
-					   (match-end line-group)))
-		  )		  
-	 )
+(setq pos (match-end 0))
+(assert-equal 233 pos)
+(assert-equal 233 (string-match dbgr-bt-re s1 pos))
+(assert-equal "vm_insnhelper.c"
+	      (substring s1 
+			 (match-beginning file-group)
+			 (match-end file-group)))
+(assert-equal "410"
+	      (substring s1 
+			 (match-beginning line-group)
+			 (match-end line-group)))
 
-(test-unit "regexp-gdb")
+(end-tests)
 
--- test/test-regexp-zshdb.el
+++ test/test-regexp-zshdb.el
@@ -1,7 +1,7 @@
-(require 'test-unit)
+(require 'test-simple)
 (load-file "../dbgr/debugger/zshdb/init.el")
 
-(test-unit-clear-contexts)
+(test-simple-start)
 
 (setq prompt-pat (gethash "prompt"        dbgr-zshdb-pat-hash))
 (setq frame-pat  (gethash "debugger-backtrace" dbgr-zshdb-pat-hash))
@@ -13,69 +13,62 @@
 				   (match-beginning 1) (match-end 1)))
 )
 
-(context "zshdb prompt matching"
-	 (tag regexp-zshdb)
-	 (specify "prompt"
-		  (prompt-match "zshdb<10> "  "10")
-		  (prompt-match	"zshdb<(5)> " "5")
-		  (prompt-match	"zshdb<<1>> " "1")
-		  )
-	 )
+(note "zshdb prompt matching")
+(prompt-match "zshdb<10> "  "10")
+(prompt-match	"zshdb<(5)> " "5")
+(prompt-match	"zshdb<<1>> " "1")
 
-(context "zshdb frame matching"
-	 (tag regexp-zshdb)
+(note "zshdb frame matching")
 
-	 (specify "debugger-backtrace"
-		  (setq s1
-			"->0 in file `/etc/apparmor/functions' at line 24
+(note "debugger-backtrace")
+(setq s1
+      "->0 in file `/etc/apparmor/functions' at line 24
 ##1 /etc/apparmor/functions called from file `/etc/init.d/apparmor' at line 35
 ##2 /etc/init.d/apparmor called from file `/usr/local/bin/zshdb' at line 129
 ")
-		  (setq frame-re (dbgr-loc-pat-regexp frame-pat))
-		  (setq num-group (dbgr-loc-pat-num frame-pat))
-		  (setq file-group (dbgr-loc-pat-file-group frame-pat))
-		  (setq line-group (dbgr-loc-pat-line-group frame-pat))
-		  (assert-equal 0 (string-match frame-re s1))
-		  (assert-equal "0" (substring s1 
-					       (match-beginning num-group)
-					       (match-end num-group)))
-		  (assert-equal "/etc/apparmor/functions"
-				(substring s1 
-					   (match-beginning file-group)
-					   (match-end file-group)))
-		  (assert-equal "24"
-				(substring s1 
-					   (match-beginning line-group)
-					   (match-end line-group)))
-		  (setq pos (match-end 0))
-		  
-		  (assert-equal 49 (string-match frame-re s1 pos))
-		  (assert-equal "1" (substring s1 
-					       (match-beginning num-group)
-					       (match-end num-group)))
-		  (assert-equal "/etc/init.d/apparmor"
-				(substring s1 
-					   (match-beginning file-group)
-					   (match-end file-group)))
-		  (assert-equal "35"
-				(substring s1 
-					   (match-beginning line-group)
-					   (match-end line-group)))
-		  (setq pos (match-end 0))
-		  (assert-equal 128 (string-match frame-re s1 pos))
-		  (assert-equal "2" (substring s1 
-					       (match-beginning num-group)
-					       (match-end num-group)))
-		  (assert-equal "/usr/local/bin/zshdb"
-				(substring s1 
-					   (match-beginning file-group)
-					   (match-end file-group)))
-		  (assert-equal "129"
-				(substring s1
-					   (match-beginning line-group)
-					   (match-end line-group)))
-		  )
-	 )
+(setq frame-re (dbgr-loc-pat-regexp frame-pat))
+(setq num-group (dbgr-loc-pat-num frame-pat))
+(setq file-group (dbgr-loc-pat-file-group frame-pat))
+(setq line-group (dbgr-loc-pat-line-group frame-pat))
+(assert-equal 0 (string-match frame-re s1))
+(assert-equal "0" (substring s1 
+			     (match-beginning num-group)
+			     (match-end num-group)))
+(assert-equal "/etc/apparmor/functions"
+	      (substring s1 
+			 (match-beginning file-group)
+			 (match-end file-group)))
+(assert-equal "24"
+	      (substring s1 
+			 (match-beginning line-group)
+			 (match-end line-group)))
+(setq pos (match-end 0))
 
-(test-unit "regexp-zshdb")
+(assert-equal 49 (string-match frame-re s1 pos))
+(assert-equal "1" (substring s1 
+			     (match-beginning num-group)
+			     (match-end num-group)))
+(assert-equal "/etc/init.d/apparmor"
+	      (substring s1 
+			 (match-beginning file-group)
+			 (match-end file-group)))
+(assert-equal "35"
+	      (substring s1 
+			 (match-beginning line-group)
+			 (match-end line-group)))
+(setq pos (match-end 0))
+(assert-equal 128 (string-match frame-re s1 pos))
+(assert-equal "2" (substring s1 
+			     (match-beginning num-group)
+			     (match-end num-group)))
+(assert-equal "/usr/local/bin/zshdb"
+	      (substring s1 
+			 (match-beginning file-group)
+			 (match-end file-group)))
+(assert-equal "129"
+	      (substring s1
+			 (match-beginning line-group)
+			 (match-end line-group)))
+
+(end-tests)
 
