3cd7554af8942d81899d2cf09e7e332442afcf58
rocky <rocky@gnu.org>
2012-09-03 08:47:57 -0400

    Untabify

--- dbgr/common/bp.el
+++ dbgr/common/bp.el
@@ -16,7 +16,7 @@
       ;; NOTE: if you don't see the icon, check the that the window margin
       ;; is not nil.
       (defconst dbgr-bp-xpm-data
-	"/* XPM */
+        "/* XPM */
 static char *magick[] = {
 /* columns rows colors chars-per-pixel */
 \"10 10 2 1\",
@@ -67,24 +67,24 @@ static char *magick[] = {
   "PBM data used for disabled breakpoint icon.")
 
       (setq dbgr-bp-enabled-icon
-	    (find-image `((:type xpm :data
-				 ,dbgr-bp-xpm-data
-				 :ascent 100 :pointer hand)
-			  (:type pbm :data
-				 ,dbgr-bp-enabled-pbm-data
-				 :ascent 100 :pointer hand))))
+            (find-image `((:type xpm :data
+                                 ,dbgr-bp-xpm-data
+                                 :ascent 100 :pointer hand)
+                          (:type pbm :data
+                                 ,dbgr-bp-enabled-pbm-data
+                                 :ascent 100 :pointer hand))))
 
       ;; For seeing what dbgr-bp-enabled-icon looks like:
       ;; (insert-image dbgr-bp-enabled-icon)
       
       (setq dbgr-bp-disabled-icon
-	    (find-image `((:type xpm :data
-				 ,dbgr-bp-xpm-data
-				 :conversion disabled ;; different than 'enabled'
-				 :ascent 100 :pointer hand)
-			  (:type pbm :data
-				 ,dbgr-bp-disabled-pbm-data
-				 :ascent 100 :pointer hand))))
+            (find-image `((:type xpm :data
+                                 ,dbgr-bp-xpm-data
+                                 :conversion disabled ;; different than 'enabled'
+                                 :ascent 100 :pointer hand)
+                          (:type pbm :data
+                                 ,dbgr-bp-disabled-pbm-data
+                                 :ascent 100 :pointer hand))))
       ))
       
 ;; For seeing what dbgr-bp-enabled-icon looks like:
@@ -94,10 +94,10 @@ static char *magick[] = {
   "Record bp information for location LOC."
   (if (dbgr-loc? loc) 
       (let* ((marker (dbgr-loc-marker loc))
-	     (bp-num (dbgr-loc-num loc))
-	     )
-	(dbgr-bp-put-icon marker 't bp-num)
-	)
+             (bp-num (dbgr-loc-num loc))
+             )
+        (dbgr-bp-put-icon marker 't bp-num)
+        )
     )
 )
 
@@ -109,25 +109,25 @@ BP-NUM is 5 the overlay string is be 'B5:' If ENABLED is false
 then the overlay string is 'b5:'. Breakpoint text properties are
 also attached to the icon via its display string."
   (let ((enabled-str)
-	(buf (or opt-buf (current-buffer)))
-	(bp-num-str
-	 (cond 
-	  ((or (not bp-num) (not (numberp bp-num))) ":")
-	  ('t (format "%d:" bp-num))))
-	(brkpt-icon)
-	(bp-str)
-	(help-string "mouse-1: clear bkpt, mouse-3: enable/disable bkpt")
-	)
+        (buf (or opt-buf (current-buffer)))
+        (bp-num-str
+         (cond 
+          ((or (not bp-num) (not (numberp bp-num))) ":")
+          ('t (format "%d:" bp-num))))
+        (brkpt-icon)
+        (bp-str)
+        (help-string "mouse-1: clear bkpt, mouse-3: enable/disable bkpt")
+        )
     (with-current-buffer buf
       (if enabled 
-	  (progn 
-	    (setq enabled-str "B")
-	    (setq brkpt-icon dbgr-bp-enabled-icon)
-	    )
-	(progn
-	  (setq enabled-str "b")
-	  (setq brkpt-icon dbgr-bp-disabled-icon)
-	  ))
+          (progn 
+            (setq enabled-str "B")
+            (setq brkpt-icon dbgr-bp-enabled-icon)
+            )
+        (progn
+          (setq enabled-str "b")
+          (setq brkpt-icon dbgr-bp-disabled-icon)
+          ))
       ;; Create alternate display string and attach
       ;; properties it.
       (setq bp-str (concat enabled-str bp-num-str))
@@ -141,17 +141,17 @@ also attached to the icon via its display string."
       ;; nil, the image doesn't get displayed, so make sure it is large
       ;; enough.
       (let ((window (get-buffer-window (current-buffer) 0)))
-	(if window
-	    (set-window-margins window 2)
-	  ;; FIXME: This is all crap, but I don't know how to fix.
-	  (let ((buffer-save (window-buffer (selected-window))))
-	    (set-window-buffer (selected-window) (current-buffer))
-	    (set-window-margins (selected-window) 2)
-	    (set-window-buffer (selected-window) buffer-save))
-	  ))
+        (if window
+            (set-window-margins window 2)
+          ;; FIXME: This is all crap, but I don't know how to fix.
+          (let ((buffer-save (window-buffer (selected-window))))
+            (set-window-buffer (selected-window) (current-buffer))
+            (set-window-margins (selected-window) 2)
+            (set-window-buffer (selected-window) buffer-save))
+          ))
       (dbgr-bp-remove-icons pos)
       (if (display-images-p) 
-	  (put-image brkpt-icon pos bp-str 'left-margin))
+          (put-image brkpt-icon pos bp-str 'left-margin))
       )
     )
   )
@@ -166,16 +166,16 @@ overlay for a before-string property containing one we normally set.
 "
   (interactive "r")
   (let* ((begin-pos (or opt-begin-pos (point)))
-	 (end-pos (or opt-end-pos begin-pos))
-	)
+         (end-pos (or opt-end-pos begin-pos))
+        )
     (dolist (overlay (overlays-in begin-pos end-pos))
       ;; We determine if this overlay is one we set by seeing if the 
       ;; string in its 'before-string property has a 'dbgr-bptno property
       (let ((before-string (overlay-get overlay 'before-string)))
-	(when (and before-string (get-text-property 0 'dbgr-bptno before-string))
-	  (delete-overlay overlay)
-	  )
-	)
+        (when (and before-string (get-text-property 0 'dbgr-bptno before-string))
+          (delete-overlay overlay)
+          )
+        )
       )
     )
   )
