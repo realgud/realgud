1f9d1437b22fc9f44e1fc417e77e064a56e073e5
expez <expez@expez.com>
2013-03-30 18:15:26 +0100

    Fix typo in package declaration.

--- emacs-dbgr-pkg.el
+++ emacs-dbgr-pkg.el
@@ -1,4 +1,4 @@
 (define-package "emacs-dbgr"
   "0.1.0"
   "A modular framework for interacting with external debuggers."
-  '((change-loc "0.1.0") (load-relative "0.2.0")))
+  '((loc-changes "0.1.0") (load-relative "0.2.0")))
