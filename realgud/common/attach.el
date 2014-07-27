0fb8ec12fbb2d441802361780746e8b68fc882a7
rocky <rocky@gnu.org>
2014-07-27 03:26:38 -0400

    Commands to associate a source buffer to a command buffer and vice versa.

---
+++ realgud/common/attach.el
@@ -0,0 +1,52 @@
+;;; Copyright (C) 2014 Rocky Bernstein <rocky@gnu.org>
+;;; Emacs Commands to associate or attach a source buffer to a command
+;;; buffer and vice versa.
+
+(eval-when-compile (require 'cl))
+(require 'load-relative)
+(require-relative-list  '("buffer/command" "buffer/source")
+			"realgud-buffer-")
+
+(declare-function realgud-cmdbuf-add-srcbuf           'realgud-buffer-command)
+(declare-function realgud-cmdbuf?                     'realgud-buffer-command)
+(declare-function realgud-srcbuf-init-or-update       'realgud-source)
+
+(defun realgud:attach-source-buffer(srcbuf)
+  "Associate a source buffer with the current command buffer"
+  (unless (realgud-cmdbuf?)
+    (error "The command only works inside a command buffer"))
+  (unless (get-buffer-process (current-buffer))
+    (warn "Can't find a process for command buffer %s" (current-buffer)))
+
+  (interactive "bsource buffer: ")
+  (let* ((cmdbuf (current-buffer))
+	 (shortkey-mode? (realgud-sget 'cmdbuf-info 'src-shortkey?)))
+    (if (stringp srcbuf) (setq srcbuf (get-buffer srcbuf)))
+    (realgud-cmdbuf-add-srcbuf srcbuf)
+    (realgud-srcbuf-init-or-update srcbuf cmdbuf)
+    (if shortkey-mode?
+	(with-current-buffer srcbuf
+	  (realgud-short-key-mode-setup 't))
+      )
+    )
+  )
+
+(defun realgud:attach-command-buffer(cmdbuf)
+  "Associate a command buffer with the current source buffer"
+
+  (interactive "bcommand buffer: ")
+  (if (stringp cmdbuf) (setq cmdbuf (get-buffer cmdbuf)))
+  (let* ((srcbuf (current-buffer))
+	 (shortkey-mode?))
+    (with-current-buffer cmdbuf
+      (unless (realgud-cmdbuf?)
+	(error "The buffer is not a command buffer"))
+      (unless (get-buffer-process (current-buffer))
+	(warn "Can't find a process for command buffer %s" (current-buffer)))
+      (setq shortkey-mode? (realgud-sget 'cmdbuf-info 'src-shortkey?)))
+    (realgud-cmdbuf-add-srcbuf srcbuf)
+    (realgud-srcbuf-init-or-update srcbuf cmdbuf)
+    (if shortkey-mode? (realgud-short-key-mode-setup 't)))
+  )
+
+(provide-me "realgud-")
