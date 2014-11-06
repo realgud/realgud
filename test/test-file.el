6fac96332bd2ce8ef21b21a0b041209001ec3a49
rocky <rocky@gnu.org>
2014-11-06 09:57:14 -0500

    Sync zshdb with bashdb. Get column numbers from source text if we can.

--- test/test-file.el
+++ test/test-file.el
@@ -2,9 +2,12 @@
 (load-file "../realgud/common/loc.el")
 (load-file "../realgud/common/file.el")
 
+;; Note the below line number is tested so it must match what's listed
+;; below.
 (declare-function __FILE__                     'require-relative)
+
 (declare-function realgud-file-loc-from-line   'realgud-file)
-(declare-function realgud-file-line-count      'realgud-file)
+(declare-function realgud:file-line-count      'realgud-file)
 (declare-function realgud-loc?(loc))
 
 (test-simple-start)
@@ -12,6 +15,7 @@
 (eval-when-compile
   (defvar realgud-file-remap)
   (defvar test-filename)
+  (defvar test-file-loc)
   (defvar remap-filename)
   (defvar old-compilation-find-file)
 )
@@ -22,13 +26,13 @@
 
 (setq test-filename (symbol-file 'test-simple))
 
-(note "realgud-file-line-count")
+(note "realgud:file-line-count")
 
 (assert-nil
- (realgud-file-line-count "not-found-file")
+ (realgud:file-line-count "not-found-file")
  "File not found")
 
-(assert-t (integerp (realgud-file-line-count test-filename))
+(assert-t (integerp (realgud:file-line-count test-filename))
 	  "File found")
 
 
@@ -36,9 +40,11 @@
 
 (fset 'compilation-find-file (lambda(mark test-filename opt)
 			       (get-buffer "*scratch*")))
-(assert-t (equal (realgud-file-loc-from-line
+(assert-equal
+ "File named `not-found-file' not readable"
+ (realgud-file-loc-from-line
 		  "not-found-file" 5 (make-marker))
-		 "File named `not-found-file' not readable"))
+ )
 
 (assert-t (stringp (realgud-file-loc-from-line test-filename 5.5))
 	  "invalid real line number")
@@ -46,24 +52,32 @@
 (assert-t (stringp (realgud-file-loc-from-line test-filename -1))
 	  "negative number")
 
+(note "realgud-file-loc-from-line information")
+
 (assert-t (stringp (realgud-file-loc-from-line test-filename 10001))
 	  "Line number too large for file")
 
-(assert-t (realgud-loc?
-	   (realgud-file-loc-from-line test-filename 30))
-	  "Ok loc creation - no cmd marker")
+(setq test-file-loc (realgud-file-loc-from-line (__FILE__) 5 nil ";; Note"))
+(assert-t (realgud-loc? test-file-loc)
+			"Ok loc creation - no cmd marker")
 
 (assert-t (realgud-loc?
 	   (realgud-file-loc-from-line test-filename 30 (make-marker)))
 	  "Ok loc creation - cmd marker")
 
+(assert-equal 5 (realgud-loc-line-number test-file-loc))
+(assert-equal 0 (realgud-loc-column-number test-file-loc))
+(assert-equal (__FILE__) (realgud-loc-filename test-file-loc))
+
 (note "realgud-file-loc-from-line remapping")
 
 (setq remap-filename " bogus remap-filename.el")
 
-(assert-t (equal (realgud-file-loc-from-line
+(assert-equal
+ (format "File named `%s' not readable" remap-filename)
+ (realgud-file-loc-from-line
 		  remap-filename 5 (make-marker))
-		 (format "File named `%s' not readable" remap-filename)))
+		 )
 
 
 (puthash remap-filename test-filename realgud-file-remap)
@@ -71,6 +85,27 @@
 (assert-t (realgud-loc?
 	   (realgud-file-loc-from-line remap-filename 30))
 	  "Ok loc creation with remap - no cmd marker")
+
+(assert-equal
+ 18
+ (realgud:file-column-from-string (__FILE__) 7 "__FILE__")
+ "Should find string in file/line and get column"
+ )
+
+(assert-nil
+ (realgud:file-column-from-string (__FILE__) 5 "__FILE__")
+ "Should not find string at this line in located file"
+ )
+
+(assert-nil
+ (realgud:file-column-from-string (concat (__FILE__) "FOO") 7 "__FILE__")
+ "Should not find file"
+ )
+
+(assert-nil
+ (realgud:file-column-from-string (__FILE__) 10000 "__FILE__")
+ "Should not find line in file at all"
+ )
 
 (fset 'compilation-find-file old-compilation-find-file)
 
