2cfd4a9c48f4d418819af0ec24079f50bfe664ca
rocky <rocky@gnu.org>
2014-12-16 16:34:23 -0500

    Release 1.0

--- realgud.el
+++ realgud.el
@@ -1,7 +1,7 @@
 ;;; realgud.el --- A modular front-end for interacting with external debuggers
 
 ;; Author: Rocky Bernstein
-;; Version: 0.2.2
+;; Version: 1.0
 ;; Package-Requires: ((load-relative "20130410") (list-utils "20140508") (loc-changes "20130723") (test-simple  "20130710"))
 ;; URL: http://github.com/rocky/emacs-dbgr
 ;; Compatibility: GNU Emacs 24.x
