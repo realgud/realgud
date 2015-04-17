ea27a82d5f65dc85c81ec8925e27817d5b895f1e
rocky <rocky@gnu.org>
2015-04-17 13:40:39 -0400

    More copyright assignments to FSF

--- realgud/debugger/gdb/track-mode.el
+++ realgud/debugger/gdb/track-mode.el
@@ -17,7 +17,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;;; gdb tracking a comint or eshell buffer.
+;; gdb tracking a comint or eshell buffer.
 
 (eval-when-compile (require 'cl))
 (require 'load-relative)
