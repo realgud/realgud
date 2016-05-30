eaae9f169bc67205fdc0cf872b22b14b4bc6ebd7
rocky <rocky@gnu.org>
2016-05-29 21:04:21 -0400

    Update commentary; e.g. link to debuggers handled

--- realgud.el
+++ realgud.el
@@ -41,61 +41,19 @@
 ;; Oh, and because global variables are largely banned, we can support
 ;; several simultaneous debug sessions.
 ;;
-;; See URL `https://github.com/rocky/emacs-dbgr/wiki/Features' for a list
+;; See URL `https://github.com/rocky/emacs-dbgr/wiki/Features' for
 ;; features.
 
-;;
-;; The debuggers we currently support are:
+;; See URL `https://github.com/rocky/emacs-dbgr/wiki/Debuggers-Supported' for
+;; debuggers we can handle.
 
-;;   NAME           INVOCATION**  WHAT
-;;   -----------------------------------
-;;   bashdb         bashdb         bash
-;;   Devel::Trepan  trepan.pl      Perl5
-;;   gdb            realgud:gdb    gdb
-;;   gub            gub            Go SSA debugger
-;;   jdb            realgud:jdb    Java jdb debugger
-;;   kshdb          kshdb          Korn Shell 93u+
-;;   nodejs         nodejs         node.js javascript debugger
-;;   pdb            realgud:pdb    stock C Python debugger
-;;   ipdb           realgud:ipdb   iPython debugger
-;;   perldb         realgud:perldb stock Perl5 debugger
-;;   remake         remake         GNU Make
-;;   ruby-debug     rdebug         Ruby
-;;   trepanning     trepan         trepanning debugger for a patched Ruby 1.9
-;;   trepanjs       trepanjs       trepanning debugger for nodejs and V8
-;;   trepan2        trepan2        trepanning debugger for Python 2.x
-;;   trepan3k       trepan3k       trepanning debugger for Python 3.x
-;;   zshdb          zshdb          Zsh
-;;
-;; The following older packages can be found in the realgud-older-debuggers
-;; package:
-
-;;   pydb           pydb           slighly enhanced pdb for Python 2.x
-;;   pydbgr         pydbgr         obsolete trepanning debugger for Python 2.x
-;;   rb8-trepanning trepan8        MRI Ruby 1.8 and an unpatched YARV 1.9
-;;   rbx-trepanning trepanx        trepanning debugger for Rubinius Ruby
-
-;; The following debuggers are found in separate individual packages:
-
-;;   byebug         realgud:byebug Ruby debugger byebug
-;;   pry            realgud:pry    Ruby debugger pry
-;;
-;;
-;;
-
-;; **gdb, jdb, perldb, pdb invocations require the realgud: preface to
-;; disambiguate it from older, preexisting Emacs commands in `gud'.
-;; The other invocations also accept realgud: prefaces,
-;; e.g. realgud:bashdb or realgud:rdebug.  Alas there are older
-;; obsolete (i.e. often written by me) Emacs packages out there for
-;; bashdb, kshdb, nodejs, pydb, rdebug, zshdb.
-
-;; If you don't see your favorite debugger above, see URL
+;; If you don't see your favorite debugger, see URL
 ;; `https://github.com/rocky/emacs-dbgr/wiki/How-to-add-a-new-debugger/'
 ;; for how you can add your own.
 
 ;; The debugger is run out of a comint process buffer, or you can use
-;; a `realgud-track-mode' inside an existing shell.
+;; a `realgud-track-mode' inside an existing comint shell, shell, or
+;; eshell buffer.
 
 ;; To install you will need a couple of other Emacs packages
 ;; installed.  If you install via melpa (`package-install') or
