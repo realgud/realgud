2316417ef3580028e8456cf1b042d906b715c8fe
rocky <rocky@gnu.org>
2016-06-27 13:28:06 -0400

    Reassign more copyrights to FSF

--- realgud/debugger/trepan2/track-mode.el
+++ realgud/debugger/trepan2/track-mode.el
@@ -1,6 +1,21 @@
-;; Copyright (C) 2010, 2012-2015 Rocky Bernstein <rocky@gnu.org>
+;; Copyright (C) 2010-2012, 2014-2016 Software Foundation, Inc
+
+;; Author: Rocky Bernstein <rocky@gnu.org>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
-;; Python "trepan2" Debugger tracking in a comint buffer.
+;; Python "trepan2" Debugger tracking in a comint or eshell buffer.
 
 (eval-when-compile (require 'cl))
 (require 'load-relative)
--- realgud/debugger/trepan2/trepan2.el
+++ realgud/debugger/trepan2/trepan2.el
@@ -1,5 +1,23 @@
-;;; Copyright (C) 2010-2012, 2014-2015 Rocky Bernstein <rocky@gnu.org>
-;;  `trepan2' Main interface to trepan2 via Emacs
+;; Copyright (C) 2010-2012, 2014-2015 Software Foundation, Inc
+
+;; Author: Rocky Bernstein <rocky@gnu.org>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; trepan2: Python 2.5 - 2.7 for 3.0+ see trepan3k
+;; Main interface to trepan2 via Emacs
+
 (require 'load-relative)
 (require-relative-list '("../../common/helper") "realgud-")
 (require-relative-list '("../../common/run")    "realgud:")
