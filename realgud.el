b7a7fe924217931332915d457928c6851db4a636
rocky <rocky@gnu.org>
2016-07-30 10:35:50 -0400

    Release 1.4.0 which will be on melpa stable and elpa

--- realgud.el
+++ realgud.el
@@ -1,7 +1,7 @@
 ;;; realgud.el --- A modular front-end for interacting with external debuggers
 
 ;; Author: Rocky Bernstein
-;; Version: 1.3.1
+;; Version: 1.4.0
 ;; Package-Requires: ((load-relative "1.0") (list-utils "0.4.2") (loc-changes "1.1") (test-simple  "1.0"))
 ;; URL: http://github.com/realgud/realgud/
 ;; Compatibility: GNU Emacs 24.x
