3cb53e0c966f48358a009702f74e2bea337424a6
rocky <rocky@gnu.org>
2016-07-30 07:30:26 -0400

    Assign more copyrights to FSF

--- realgud/common/follow.el
+++ realgud/common/follow.el
@@ -1,4 +1,17 @@
-;;; Copyright (C) 2015 Rocky Bernstein <rocky@gnu.org>
+;; Copyright (C) 2015 Free Software Foundation, Inc
+
+;; Author: Rocky Bernstein <rocky@gnu.org>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
 ;;; Follows or goto's something
 (require 'load-relative)
 
--- realgud/debugger/trepan3k/core.el
+++ realgud/debugger/trepan3k/core.el
@@ -1,4 +1,4 @@
-;; Copyright (C) 2010, 2012-2015 Software Foundation, Inc
+;; Copyright (C) 2010, 2012-2015 Free Software Foundation, Inc
 
 ;; Author: Rocky Bernstein <rocky@gnu.org>
 
