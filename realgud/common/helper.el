bbb86b7010cac192f05c274c57115423f8cc5cc7
rocky <rocky@gnu.org>
2016-08-01 14:44:12 -0400

    Wrong require - thanks stephan

--- realgud/common/helper.el
+++ realgud/common/helper.el
@@ -12,7 +12,7 @@
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
-(require 'cl)
+(require 'cl-lib)
 
 ;;; Miscellaneous utility functions
 (require 'load-relative)
