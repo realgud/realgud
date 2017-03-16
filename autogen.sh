da5fbd6e461211a21c382a09abd5b94be3340560
rocky <rb@dustyfeet.com>
2017-03-16 04:12:14 -0400

    Fix up automake install

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# An autoconf setup script.
+# From inside emacs, make sure test-simple is installed and then
+# Press C-x C-e at the end of the next line run this script
+# (test-simple-run "EMACSLOADPATH=%s ./autogen.sh"  (mapconcat 'identity load-path ":"))
 ln -fs README.md README
 touch common.mk
 autoreconf -vi && \
