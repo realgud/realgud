2fad0c4232869b54e670248d21db19b6bb594175
denin <dvzubarev@yandex.ru>
2017-12-03 16:07:11 +0300

    Add test case for an error when realgud:backtrace-init is hanging at startup
    see #192

--- test/test-bt-trepan2.el
+++ test/test-bt-trepan2.el
@@ -4,15 +4,19 @@
 (require 'test-simple)
 (require 'load-relative)
 (load-file "./bt-helper.el")
+(load-file "./regexp-helper.el")
 (load-file "../realgud/debugger/trepan2/init.el")
 
 (declare-function setup-bt 'realgud-bt-helper)
+(declare-function setup-regexp-vars 'regexp-helper)
 (declare-function __FILE__ 'load-relative)
 
 (test-simple-start)
 
 (eval-when-compile
   (defvar temp-bt)
+  (defvar realgud-pat-bt)
+  (defvar realgud:trepan2-pat-hash)
 )
 
 (setq temp-bt
@@ -40,4 +44,34 @@
 		  (get-text-property (point) 'face))
     )
   )
+
+
+(setup-regexp-vars realgud:trepan2-pat-hash)
+(setq realgud-pat-bt  (gethash "debugger-backtrace"
+                               realgud:trepan2-pat-hash))
+
+
+(let* ((triple
+        (realgud:backtrace-add-text-properties
+         realgud-pat-bt ""
+         "->0 gcd(a=3, b=5) called from file '/test/gcd.py' at line 28
+##1 <module> exec() '/test/gcd.py' at line 41"
+         "->"))
+       (string-with-props (car triple)))
+  (dolist (pair
+           '(
+             ("->0" . (0 . 28) )
+             ("##1" . (1 . 41) )
+             ))
+    (string-match (car pair) string-with-props)
+    (assert-equal (cddr pair)
+                  (realgud-loc-line-number (get-text-property
+                                            (match-beginning 0) 'loc
+                                            string-with-props)))
+
+    (assert-equal (cadr pair)
+                  (get-text-property
+                   (match-beginning 0) 'frame-num
+                   string-with-props))))
+
 (end-tests)
