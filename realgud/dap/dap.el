11f6d6ef994666342c59c1d69ee8764932ba3f09
813gan <813gan@protonmail.com>
2023-10-14 21:44:46 +0000

    DAP: Start integrating into rg machinery

--- realgud/dap/dap.el
+++ realgud/dap/dap.el
@@ -84,3 +84,122 @@
        :service port
        ; :filter-multibyte 't ??
        )))
+
+(defun realgud--dap-event-loop nil
+  (with-mutex (alist-get 'mutex realgud-dap-message-que)
+    (condition-wait (alist-get 'notify-var realgud-dap-message-que))
+    (let* ((msg (pop (alist-get 'que realgud-dap-message-que)))
+	   (msg-body (plist-get :body msg)))
+      )
+    ))
+
+(defun realgud--dap-start-message-loop nil
+  (with-current-buffer-safe (realgud-get-cmdbuf)
+    ; TODO register thread in cmdbuf so we can kill it
+    (make-thread (lambda nil (while 't (realgud--dap-message-handle)))
+		 "realgud message handler")
+    ))
+
+(defgroup realgud:dap nil
+  "The realgud interface to DAP debuggers."
+  :group 'realgud
+  :version "25.13") ; TODO which version??
+
+(defcustom realgud:dap-command-name
+  nil
+  "File name for executing the DAP debugger and command options.
+This should be an executable on your path, or an absolute file name.
+You should set this variable in your project's directory variables"
+  :type 'string
+  :group 'realgud:dap
+  :safe 'stringp)
+
+(defvar realgud:dap-minibuffer-history nil
+  "minibuffer history list for the DAP debuggers.")
+
+(easy-mmode-defmap dap-minibuffer-local-map
+  nil
+  "Keymap for minibuffer prompting of debugger startup command."
+  :inherit minibuffer-local-map)
+
+(defvar dap-track-mode-map
+  (make-sparse-keymap) ; TODO
+  )
+
+(defun dap-suggest-invocation (debugger-name)
+  "Suggest a pdb command invocation via `realgud-suggest-invocaton'"
+  "debugpy")
+
+(defun dap-query-cmdline (&optional opt-debugger)
+  (realgud-query-cmdline
+   'dap-suggest-invocation
+   dap-minibuffer-local-map
+   'realgud:dap-minibuffer-history
+   opt-debugger))
+
+(defun dap-track-mode-hook()
+  (if dap-track-mode
+      (progn
+        (use-local-map dap-track-mode-map)
+        (message "using dap mode map")
+        )
+    (message "dap track-mode-hook disable called")
+    )
+)
+
+(define-minor-mode dap-track-mode
+  "Minor mode for tracking pdb source locations inside a process shell via realgud. pdb is the stock Python debugger.
+
+If called interactively with no prefix argument, the mode is toggled. A prefix argument, captured as ARG, enables the mode if the argument is positive, and disables it otherwise.
+
+a process shell.
+
+\\{pdb-track-mode-map}
+"
+  :init-value nil
+  :global nil
+  :group 'realgud:dap
+  :keymap dap-track-mode-map
+  (realgud:track-set-debugger "dap")
+  (if dap-track-mode
+      (progn
+	(realgud-track-mode-setup 't)
+        (dap-track-mode-hook))
+    (progn
+      (setq realgud-track-mode nil)
+      ))
+  )
+
+(defun dap-parse-cmd-args (args)
+    ;(list interpreter-args debugger-args script-args annotate-p)
+    (list args nil nil nil))
+
+;;;###autoload
+(defun realgud:dap (&optional opt-cmd-line no-reset)
+  "Invoke the DAP debugger and start the Emacs user interface.
+
+String OPT-CMD-LINE specifies how to run pdb. You will be prompted
+for a command line is one isn't supplied.
+OPT-COMMAND-LINE is treated like a shell string; arguments are
+tokenized by `split-string-and-unquote'. The tokenized string is
+parsed by `pdb-parse-cmd-args' and path elements found by that
+are expanded using `realgud:expand-file-name-if-exists'.
+
+Normally, command buffers are reused when the same debugger is
+reinvoked inside a command buffer with a similar command. If we
+discover that the buffer has prior command-buffer information and
+NO-RESET is nil, then that information which may point into other
+buffers and source buffers which may contain marks and fringe or
+marginal icons is reset. See `loc-changes-clear-buffer' to clear
+fringe and marginal icons.
+"
+					; TODO doc
+  (interactive)
+  (realgud:run-debugger "dap" 'dap-query-cmdline
+			'dap-parse-cmd-args
+			'realgud:dap-minibuffer-history
+			opt-cmd-line no-reset) )
+
+(provide-me "realgud:dap-")
+
+;;; dap.el ends here
