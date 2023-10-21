realgud/realgud

commit d21f338ebee075d24066c2ff90f335d6592991ae

Support tramp buffers in command expansion

realgud/common/send.el
@@ -149,7 +149,8 @@ taken from current buffer, or OPT-BUFFER if non-nil.  Some
 %p and %s are replaced by an empty string if OPT-STR is nil."
   (let* ((buffer (or opt-buffer (current-buffer)))
 	 (srcbuf (realgud-get-srcbuf buffer))
-	 (src-file-name (and srcbuf (buffer-file-name srcbuf)))
+	 (src-file-name-raw (and srcbuf (buffer-file-name srcbuf)))
+         (src-file-name (if (file-remote-p src-file-name-raw) (file-remote-p src-file-name-raw 'localname) src-file-name-raw))
 	 result)
     (while (and fmt-str
 		(let ((case-fold-search nil))

commit e7b622cd8947cdf0178e7a347fdded506d27ba7d

Fix error in realgud-expand-format introduced in `d21f338`
`src-file-name-raw` could be `nil`, resulting in an error when calling `file-remote-p` and causing
this function to break.

realgud/common/send.el
@@ -150,7 +150,8 @@ taken from current buffer, or OPT-BUFFER if non-nil.  Some
   (let* ((buffer (or opt-buffer (current-buffer)))
 	 (srcbuf (realgud-get-srcbuf buffer))
 	 (src-file-name-raw (and srcbuf (buffer-file-name srcbuf)))
-         (src-file-name (if (file-remote-p src-file-name-raw) (file-remote-p src-file-name-raw 'localname) src-file-name-raw))
+         (src-file-name (and src-file-name-raw
+                             (if (file-remote-p src-file-name-raw) (file-remote-p src-file-name-raw 'localname) src-file-name-raw)))
 	 result)
     (while (and fmt-str
 		(let ((case-fold-search nil))